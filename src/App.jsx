9e6191d03d255e4e9cd6a3714faa09ced6393843
Daksh Choudhary <ch.daksh01@gmail.com>
2024-11-04 21:36:01 +0530

    added intensity

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import React from 'react'
 import "./style.css"
 import { OrbitControls } from '@react-three/drei'
 import Cylinder from './Cylinder'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
 
 const App = () => {
-  return <Canvas camera={{fov:35}}>
+  return <Canvas flat camera={{fov:35}}>
     {/* use "drei three js" for orbit controls */}
     <OrbitControls /> 
-    <ambientLight />
+    <ambientLight intensity={1.5} />
     <Cylinder />
+    <EffectComposer>
+    <Bloom
+      mipmapBlur={true}
+      intensity={10.0}
+      luminanceThreshold={0}
+      luminanceSmoothing={0}
+    />
+    </EffectComposer>
+
   </Canvas>
 }
 
